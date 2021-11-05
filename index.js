eecff981b0e7df452264bc4bc018749e3020be46
NuminousBlue <jdiehl2236@gmail.com>
2021-11-05 18:34:57 -0500

    Inquirer prompts

--- index.js
+++ index.js
@@ -1,39 +1,90 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const Employee = require('./lib/Employee');
+const Employee = require('./lib/Employee')
 const Engineer = require('./lib/Engineer')
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 
-inquirer.prompt(questions)
-     .then((answers) =>
-          transferTemplate(answers))
-     .catch((error) => console.log(error));
-
-function transferTemplate(answersObj) {
-     const genHTML = htmlTemplate(answersObj);
-     fs.writeFile('myTeam.html', genHTML, (err) => 
-     err ? console.log(err) : console.log('Creation of Team Page successful!'))
-}
 
 
+const intro = ([
+     {
+          type: 'input',
+          name: 'teamName',
+          message: 'What is the name of your team?'
+     },
+     {
+          type: 'confirm',
+          name: 'manager',
+          message: 'Does your team have a manager?',
+     },
+])
 
-const htmlTemplate = ({
-     teamName,
-}) =>
-     `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>${teamName}</title>
-</head>
-<body>
-     <h1>${teamName}</h1>
-     
-</body>
-</html>
-`
+const manPrompts = ([
+     {
+          type: 'input',
+          name: 'manName',
+          message: "What is your manager's name?"
+     },
+     {
+          type: 'input',
+          name: 'manId',
+          message: "What is your manager's ID #?"
+     },    
+     {
+          type: 'input',
+          name: 'manEmail',
+          message: "What is your manager's email address?",
+          validate: function(email) {
+               return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+          }
+     },
+     {
+          type: 'input',
+          name: 'officeNum',
+          message: "What is your manager's Office #?"
+     }
+])
+
+const engPrompts = ([
+     {
+          type: 'input',
+          name: 'engName',
+          message: "What is the name of your engineer?"
+     },
+     {
+          type: 'input',
+          name: 'engId',
+          message: "What is your engineer's ID #?"
+     },
+     {
+          type: 'input',
+          name: 'engEmail',
+          message: "What is your engineer's email address?",
+          validate: function(email) {
+               return /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+          }
+     },
+     {
+          type: 'input',
+          name: 'engGit',
+          message: "What is your engineer's GitHub username?"
+     }
+
+])
+inquirer.prompt(intro)
+     .then((answers) => {
+          console.log(answers)
+          if (answers.manager === true) {
+               inquirer.prompt(manPrompts)
+               .then((answers) => {
+                    let {manName, manId, manEmail, officeNum} = answers;
+                    let teamManager = new Manager(manName, manId, manEmail, officeNum);                   
+               })
+               .catch((error) => console.log(error))
+          }
+          else if (answers.manager === false) {
+               inquirer.prompt(engPrompts)
+          }
+     })
